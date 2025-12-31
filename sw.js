877289363d04f24970026e5072a96aaf636526f2
rac010203 <elegantmoolah0102@gmail.com>
2025-12-31 16:33:20 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "drink-tap-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
